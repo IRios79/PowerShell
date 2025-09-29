8a857156c7a7491193bc565a4762e6d0769e6f20
IRios79 <1979irr@gmail.com>
2025-09-29 11:01:33 +0200

    Nueva Sincronización

---
+++ 10.Param_Busc_Proces.ps1
@@ -0,0 +1,23 @@
+﻿# BUSCAR UN PROCESO
+
+param(
+$proceso
+)
+$f=0 #Inicia una variable como bandera
+$p=Get-Process
+$l=$p.Length
+for ($i=0; $i -lt $l; $i++)
+{
+    if ($p[$i].ProcessName -eq $proceso)
+    {
+        $f=1
+    }
+}
+if ($f -eq 1)
+{
+    Write-Host "Proceso Activo"
+}
+else
+{
+    Write-Host "Proceso NO Activo"
+}
