f03fb5f75c1b100457a1b57a15319cdb5da5e6f6
IRios79 <1979irr@gmail.com>
2025-09-29 11:08:48 +0200

    Sincronización Flags

--- 10.Param_Busc_Pro_Flags.ps1
+++ 10.Param_Busc_Pro_Flags.ps1
@@ -3,12 +3,21 @@
 param(
 $proceso
 )
+$f=0 #Inicia una variable como bandera
 $p=Get-Process
 $l=$p.Length
 for ($i=0; $i -lt $l; $i++)
 {
     if ($p[$i].ProcessName -eq $proceso)
     {
-        Write-Host "Proceso Activo"
+        $f=1
     }
+}
+if ($f -eq 1)
+{
+    Write-Host "Proceso Activo"
+}
+else
+{
+    Write-Host "Proceso NO Activo"
 }
